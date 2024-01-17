cfccf6f3b5db257020d3f336ec1e25bc62dc92dd
Nicolas Aguilera Contreras <nicolas.aguilera.co@gmail.com>
2024-01-17 17:56:02 -0500

    feat: creating EmployeeDto class

---
+++ employees/src/main/java/com/amaris/employees/dto/EmployeeDto.java
@@ -0,0 +1,31 @@
+package com.amaris.employees.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
+@Schema(name = "Employee", description = "Schema to hold Employee information")
+public class EmployeeDto {
+
+    @Schema(description = "Name of the employee")
+    @NotEmpty(message = "Name can not be null or empty")
+    private String name;
+
+    @Schema(description = "Age of the employee")
+    @NotNull(message = "Age can not be null")
+    private Integer age;
+
+    @Schema(description = "Salary of the employee")
+    @NotNull(message = "Salary can not be null")
+    private Long salary;
+
+    @Schema(description = "ID of the employee")
+    @NotNull(message = "ID can not be null")
+    private Long id;
+
+    @Schema(description = "Profile image URL of the employee")
+    private String profile_image;
+}
