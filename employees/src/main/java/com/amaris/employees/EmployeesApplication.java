9bfdd087ab427fa352a39e576809dd6cfc7188f2
Nicolás Aguilera Contreras <nicolas.aguilera@exxcellent.de>
2024-01-18 18:36:25 -0500

    feat: packaging angular app inside the SpringBoot app

--- employees/src/main/java/com/amaris/employees/EmployeesApplication.java
+++ employees/src/main/java/com/amaris/employees/EmployeesApplication.java
@@ -2,12 +2,20 @@ package com.amaris.employees;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeesApplication {
+public class EmployeesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeesApplication.class, args);
 	}
 
+	//If you need a traditional war deployment we need to extend the SpringBootServletInitializer
+	//This helps us deploy war files to Jboss containers
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
+		return springApplicationBuilder.sources(EmployeesApplication.class);
+	}
 }
