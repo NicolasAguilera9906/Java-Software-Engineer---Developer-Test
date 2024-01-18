e99bd3d029413f2391109d1ab0260afe292fada6
Nicolas Aguilera Contreras <nicolas.aguilera.co@gmail.com>
2024-01-17 21:25:39 -0500

    feat: Adding functionality to retrieve only one Employee from the API in the EmployeeAPIConsumer class

--- employees/src/main/java/com/amaris/employees/api/IEmployeeAPIConsumer.java
+++ employees/src/main/java/com/amaris/employees/api/IEmployeeAPIConsumer.java
@@ -1,6 +1,7 @@
 package com.amaris.employees.api;
 
 import com.amaris.employees.exception.EmployeeApiException;
+import com.amaris.employees.exception.ResourceNotFoundException;
 import com.amaris.employees.models.Employee;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +11,5 @@ public interface IEmployeeAPIConsumer {
 
     List<Employee> fetchEmployeesFromApi() throws EmployeeApiException.ApiException;
 
+    Employee fetchEmployeeFromApi(String employeeId) throws EmployeeApiException.ApiException, ResourceNotFoundException;
 }
--- employees/src/main/java/com/amaris/employees/api/impl/EmployeeAPIConsumer.java
+++ employees/src/main/java/com/amaris/employees/api/impl/EmployeeAPIConsumer.java
@@ -2,10 +2,12 @@ package com.amaris.employees.api.impl;
 
 import com.amaris.employees.api.IEmployeeAPIConsumer;
 import com.amaris.employees.exception.EmployeeApiException;
+import com.amaris.employees.exception.ResourceNotFoundException;
 import com.amaris.employees.models.Employee;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -38,6 +40,33 @@ public class EmployeeAPIConsumer implements IEmployeeAPIConsumer {
                 return gson.fromJson(dataArray, listType);
             } else {
                 throw new EmployeeApiException.ApiException("Failed to retrieve Employees data from the API", statusCode);
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new EmployeeApiException.ApiException("Failed to connect to the API", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        }
+    }
+
+    @Override
+    public Employee fetchEmployeeFromApi(String employeeId) throws EmployeeApiException.ApiException, ResourceNotFoundException {
+        try {
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(EMPLOYEES_API_URL + "/" + employeeId)).GET().build();
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            int statusCode = response.statusCode();
+            if (statusCode == 200) {
+                Gson gson = new Gson();
+                JsonObject jsonObject = gson.fromJson(response.body(), JsonObject.class);
+                if (jsonObject.has("data")) {
+                    JsonElement dataElement = jsonObject.get("data");
+                    return gson.fromJson(dataElement, Employee.class);
+                } else {
+                    throw new ResourceNotFoundException(String.format(employeeId));
+                }
+            } else {
+                throw new EmployeeApiException.ApiException(
+                        String.format("Failed to retrieve Employee data for ID: %s from the API", employeeId),
+                        statusCode
+                );
             }
         } catch (IOException | InterruptedException e) {
             throw new EmployeeApiException.ApiException("Failed to connect to the API", HttpStatus.INTERNAL_SERVER_ERROR.value());
