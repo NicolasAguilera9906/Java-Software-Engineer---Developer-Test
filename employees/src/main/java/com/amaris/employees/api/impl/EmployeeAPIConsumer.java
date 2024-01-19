4ce6f49e77c6f4dbbe04c6db2c2c8aabdd27f8d8
Nicolás Aguilera Contreras <nicolas.aguilera@exxcellent.de>
2024-01-18 19:38:04 -0500

    compatibility with java 1.8+

--- employees/src/main/java/com/amaris/employees/api/impl/EmployeeAPIConsumer.java
+++ employees/src/main/java/com/amaris/employees/api/impl/EmployeeAPIConsumer.java
@@ -6,18 +6,19 @@ import com.amaris.employees.exception.employee.EmployeeNotFoundException;
 import com.amaris.employees.models.Employee;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.List;
 
 import static com.amaris.employees.constants.EmployeesConstants.EMPLOYEES_API_URL;
@@ -27,24 +28,26 @@ import static com.amaris.employees.constants.EmployeesConstants.EMPLOYEE_API_URL
 public class EmployeeAPIConsumer implements IEmployeeAPIConsumer {
 
     @Override
-    public List<Employee> fetchEmployeesFromApi() throws EmployeeApiException.ApiException{
+    public List<Employee> fetchEmployeesFromApi() throws EmployeeApiException.ApiException {
         try {
-            HttpClient httpClient = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(EMPLOYEES_API_URL)).GET().build();
-            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            int statusCode = response.statusCode();
-            if (statusCode == 200) {
-                Gson gson = new Gson();
-                JsonObject jsonObject = gson.fromJson(response.body(), JsonObject.class);
-                JsonArray dataArray = jsonObject.getAsJsonArray("data");
-                Type listType = new TypeToken<List<Employee>>() {}.getType();
-                return gson.fromJson(dataArray, listType);
-            } else {
-                throw new EmployeeApiException.ApiException("Failed to retrieve Employees data from the API. " +
-                        "Response Message: " + response.body(), statusCode);
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpGet httpGet = new HttpGet(EMPLOYEES_API_URL);
 
+            try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+                int statusCode = response.getStatusLine().getStatusCode();
+
+                if (statusCode == 200) {
+                    Gson gson = new Gson();
+                    JsonObject jsonObject = gson.fromJson(EntityUtils.toString(response.getEntity()), JsonObject.class);
+                    JsonArray dataArray = jsonObject.getAsJsonArray("data");
+                    Type listType = new TypeToken<List<Employee>>() {}.getType();
+                    return gson.fromJson(dataArray, listType);
+                } else {
+                    throw new EmployeeApiException.ApiException("Failed to retrieve Employees data from the API. " +
+                            "Response Message: " + EntityUtils.toString(response.getEntity()), statusCode);
+                }
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             throw new EmployeeApiException.ApiException("Failed to connect to the API", HttpStatus.INTERNAL_SERVER_ERROR.value());
         }
     }
@@ -52,26 +55,30 @@ public class EmployeeAPIConsumer implements IEmployeeAPIConsumer {
     @Override
     public Employee fetchEmployeeFromApi(String employeeId) throws EmployeeApiException.ApiException, EmployeeNotFoundException {
         try {
-            HttpClient httpClient = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(EMPLOYEE_API_URL + "/" + employeeId)).GET().build();
-            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            int statusCode = response.statusCode();
-            if (statusCode == 200) {
-                Gson gson = new Gson();
-                JsonObject jsonObject = gson.fromJson(response.body(), JsonObject.class);
-                if (!jsonObject.get("data").isJsonNull()) {
-                    JsonElement dataElement = jsonObject.get("data");
-                    return gson.fromJson(dataElement, Employee.class);
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpGet httpGet = new HttpGet(EMPLOYEE_API_URL + "/" + employeeId);
+
+            try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+                int statusCode = response.getStatusLine().getStatusCode();
+
+                if (statusCode == 200) {
+                    Gson gson = new Gson();
+                    JsonObject jsonObject = gson.fromJson(EntityUtils.toString(response.getEntity()), JsonObject.class);
+
+                    if (!jsonObject.get("data").isJsonNull()) {
+                        JsonElement dataElement = jsonObject.get("data");
+                        return gson.fromJson(dataElement, Employee.class);
+                    } else {
+                        throw new EmployeeNotFoundException(employeeId);
+                    }
                 } else {
-                    throw new EmployeeNotFoundException(employeeId);
+                    throw new EmployeeApiException.ApiException(
+                            String.format("Failed to retrieve Employee data for ID: %s from the API", employeeId),
+                            statusCode
+                    );
                 }
-            } else {
-                throw new EmployeeApiException.ApiException(
-                        String.format("Failed to retrieve Employee data for ID: %s from the API", employeeId),
-                        statusCode
-                );
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             throw new EmployeeApiException.ApiException("Failed to connect to the API", HttpStatus.INTERNAL_SERVER_ERROR.value());
         }
     }
